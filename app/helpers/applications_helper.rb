b00135a6a69ac919a523e8cc2e9a2e6a13fb09b9
jbbarth <jeanbaptiste.barth@gmail.com>
2010-11-14 10:55:42 +0100

    Fixed ApplicationsHelper#cerbere_image helper, Application.cerbere has already been migrated to a boolean

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationsHelper
   def cerbere_image(has_cerbere, ifnot = false)
-    #TODO: migrate application.cerbere to a boolean!
-    if has_cerbere == 1
+    if has_cerbere
       image_tag("cerbere.gif")
     elsif ifnot
       image_tag("false_cerbere.gif")
