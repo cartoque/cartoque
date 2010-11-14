62a8e2e7fc644627f7dcf30f37d6c207c6972b73
jbbarth <jeanbaptiste.barth@gmail.com>
2010-11-14 09:55:57 +0100

    Improved error messages display

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -6,6 +6,7 @@ SimpleForm.setup do |config|
   # Components used by the form builder to generate a complete input. You can remove
   # any of them, change the order, or even add your own components to the stack.
   # config.components = [ :label_input, :hint, :error ]
+  config.components = [ :label_input, :hint ]
 
   # Default tag used on hints.
   # config.hint_tag = :span
