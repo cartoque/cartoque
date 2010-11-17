efc01b2a1bd8b397de0952549057909a642acb51
jbbarth <jeanbaptiste.barth@gmail.com>
2010-11-17 13:37:52 +0100

    Added sort options to applications/index

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationsController < InheritedResources::Base
+  helper_method :sort_column, :sort_direction
+
+  def collection
+    @applications ||= end_of_association_chain.order(sort_column + " " + sort_direction)
+  end
+
+  private
+  def sort_column
+    Application.column_names.include?(params[:sort]) ? params[:sort] : "nom"
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : "asc"
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, :sort => column, :direction => direction
+  end
+
+  def sortable_css_class(column)
+    column == sort_column ? "current #{sort_direction}" : "sortable"
+  end
 end
