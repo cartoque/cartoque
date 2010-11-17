87b8a251629638d39ca7d88db3d4e5f33ecd3af9
jbbarth <jeanbaptiste.barth@gmail.com>
2010-11-17 20:27:27 +0100

    Updated applications search so that they work through AJAX

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,8 @@ $(function() {
     $.getScript(this.href);
     return false;
   });
+  $("#applications_search").submit(function() {
+    $.get(this.action, $(this).serialize(), null, "script");
+    return false;
+  });
 });
