9272fc530cb2a468e04d8255c8f6f3a3b6789085
jbbarth <jeanbaptiste.barth@gmail.com>
2010-11-17 14:38:53 +0100

    Sort applications through AJAX

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationsController < InheritedResources::Base
+  respond_to :html, :js
+
   helper_method :sort_column, :sort_direction
 
   def collection
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $("#applications th a").live("click", function() {
+    $.getScript(this.href);
+    return false;
+  });
+});
