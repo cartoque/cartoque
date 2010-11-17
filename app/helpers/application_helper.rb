501b9a58321ac559d8832570631811a4a5eac532
jbbarth <jeanbaptiste.barth@gmail.com>
2010-11-17 13:57:22 +0100

    Fixed search and sort messing with each other

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, params.merge(:sort => column, :direction => direction)
   end
 
   def sortable_css_class(column)
