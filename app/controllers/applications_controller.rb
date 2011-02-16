e02c073cd4c00903f5b3670838b28638141a102e
jbbarth <jeanbaptiste.barth@gmail.com>
2011-02-16 12:58:40 +0100

    Added Application#machines in show xml output ; needs a dirty patch for rails ticket #4840, which should be solved in Rails 3.0.5

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,6 +3,12 @@ class ApplicationsController < InheritedResources::Base
 
   helper_method :sort_column, :sort_direction
 
+  def show
+    super do |format|
+      format.xml { render :xml => @application.to_xml(:include => :machines) }
+    end
+  end
+
   def collection
     @applications ||= end_of_association_chain.search(params[:search]).order(sort_column + " " + sort_direction)
   end
@@ -14,5 +20,13 @@ class ApplicationsController < InheritedResources::Base
 
   def sort_direction
     %w(asc desc).include?(params[:direction]) ? params[:direction] : "asc"
+  end
+end
+
+#super dirty patch for rails ticket #4840
+#TODO: remove it when Rails 3.0.5 is here...
+class NilClass
+  def type
+    nil
   end
 end
