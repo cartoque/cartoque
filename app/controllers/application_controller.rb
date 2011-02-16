623045bc5b46837147c0560cb2b756e9e22dd429
jbbarth <jeanbaptiste.barth@gmail.com>
2011-02-16 09:04:38 +0100

    Rescue from ActiveRecord::RecordNotFound errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    status = 404
+    respond_to do |format|
+      format.html { render :text => "These are not the droids you're looking for", :status => status }
+      format.atom { head status }
+      format.xml { head status }
+      format.js { head status }
+      format.json { head status }
+    end
+  end
 end
