efe1b7fd18ffb34129f5b5475e203249649803e9
jbbarth <jeanbaptiste.barth@gmail.com>
2011-03-08 19:20:06 +0100

    Rename RHEL operating systems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110217184616) do
+ActiveRecord::Schema.define(:version => 20110308100406) do
 
   create_table "applications", :force => true do |t|
     t.string  "nom",                      :default => "",    :null => false
