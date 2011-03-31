4ac845b8371a957205c85cf4796ede116822df06
jbbarth <jeanbaptiste.barth@gmail.com>
2011-03-31 10:27:34 +0200

    Display database reports even in databases/show view

--- app/helpers/databases_helper.rb
+++ app/helpers/databases_helper.rb
@@ -1,12 +1,28 @@
+# encoding: utf-8
+
 module DatabasesHelper
   def database_nodes(database)
-    html = "<strong>#{database.name}</strong>"
+    html = "<strong>#{link_to database.name, database}</strong>"
     if database.machines.any? && database.machines.map(&:nom) != [database.name]
       html << %(<ul class="database_nodes">)
       html << database.machines.map(&:nom).sort.map{|n| "<li>#{n}</li>"}.join("\n")
       html << "</ul>"
     end
     html.html_safe
+  end
+
+  def database_headers_for(database_type)
+    if database_type == "postgres"
+      %(<th>PgCluster</th>
+        <th>IP:Port</th>
+        <th style="text-align:left;">Bases<span style="float:right;padding-left:1em">Taille(Go)</span></th>
+        <th>Stockage</th>).html_safe
+    elsif database_type == "oracle"
+      %(<th>Instance</th>
+        <th>IP:Port</th>
+        <th style="text-align:left;">Schémas<span style="float:right;padding-left:1em">Taille(Go)</span></th>
+        <th>Stockage</th>).html_safe
+    end
   end
 
   def display_size(size)
