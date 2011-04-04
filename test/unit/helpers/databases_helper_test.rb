2439a137b8b356868e0718b3faedf205ac9b9505
jbbarth <jeanbaptiste.barth@gmail.com>
2011-04-04 10:22:43 +0200

    Added a link to toggle summary view in databases/index

--- test/unit/helpers/databases_helper_test.rb
+++ test/unit/helpers/databases_helper_test.rb
@@ -20,4 +20,23 @@ class DatabasesHelperTest < ActionView::TestCase
     assert_equal "<abbr title=\"1.0Mo\">0.0</abbr>", display_size(1024**2)
     assert_equal "2.5", display_size(2.5*1024**3)
   end
+
+  context "#databases_summary" do
+    should "return empty string if databases list is empty" do
+      assert_equal "", databases_summary([])
+    end
+
+    should "return databases where size >= total_size / 6" do
+      databases = {"big_one" => 19, "little_one" => 2, "big_two" => 15, "little_two" => 3}
+      assert databases_summary(databases).match(/^big_one, big_two/)
+      assert ! databases_summary(databases).include?("little")
+    end
+
+    should "return first two databases if none is greater than total_size / 6" do
+      databases = {"big_one" => 2, "big_two" => 2}
+      (1..8).each { |i| databases[i.to_s] = 1 }
+      assert databases_summary(databases).match(/^big_one, big_two/)
+      assert ! databases_summary(databases).include?("little")
+    end
+  end
 end
