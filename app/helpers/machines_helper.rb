0f28e323bfe65fe49b9a79f1e53d3fca62194c27
jbbarth <jeanbaptiste.barth@gmail.com>
2011-04-11 12:42:15 +0200

    Added a color to 'Machine#maintained_until' dates (red if < 6 months from now, orange if < 12 months, no color else)

--- app/helpers/machines_helper.rb
+++ app/helpers/machines_helper.rb
@@ -1,2 +1,13 @@
 module MachinesHelper
+  def maintenance_limit(date)
+    return "" unless date
+    months_before_end = ((date - Date.today) / 30).to_f
+    if months_before_end <= 6
+      content_tag(:span, l(date), :class => "maintenance-critical")
+    elsif months_before_end <= 12
+      content_tag(:span, l(date), :class => "maintenance-warning")
+    else
+      l(date)
+    end
+  end
 end
