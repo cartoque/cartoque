1168ad908aa40f5bc8b0ea583a8d41e1b89217ed
jbbarth <jeanbaptiste.barth@gmail.com>
2011-04-20 11:04:19 +0200

    Auto-migrate database clusters from our legacy database

---
+++ db/migrate/20110420085516_detect_sgbd_from_old_database.rb
@@ -0,0 +1,16 @@
+class DetectSgbdFromOldDatabase < ActiveRecord::Migration
+  def self.up
+    servers = Machine.where("nom like 'sgbd%'")
+    servers.map do |s|
+      s.nom.gsub(/-\d*$/,"")
+    end.uniq.sort.each do |cluster|
+      d = Database.new(:name => cluster)
+      d.machines = servers.select{|s| s.nom.starts_with?(cluster)}
+      d.database_type = (cluster.match(/m2$|a$/i) ? "postgres" : "oracle")
+      d.save
+    end
+  end
+
+  def self.down
+  end
+end
