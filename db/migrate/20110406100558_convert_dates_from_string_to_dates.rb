22a2f730997399a40d3270b5ce23e70a47b728b3
jbbarth <jeanbaptiste.barth@gmail.com>
2011-04-20 11:58:04 +0200

    Last try at avoiding 'invalid date' errors was not so good

--- db/migrate/20110406100558_convert_dates_from_string_to_dates.rb
+++ db/migrate/20110406100558_convert_dates_from_string_to_dates.rb
@@ -5,7 +5,10 @@ class ConvertDatesFromStringToDates < ActiveRecord::Migration
         value = m.send(old_field)
         unless value.blank?
           value = "#{$1}/20#{$2}" if value.match %r{(.*\d\d)/(\d\d)$}
-          m.send("#{new_field}=", (Date.parse(value) rescue nil))
+          begin
+            m.send("#{new_field}=", Date.parse(value))
+          rescue ArgumentError
+          end
           m.save
         end
       end
