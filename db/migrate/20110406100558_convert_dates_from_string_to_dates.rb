e052de8e974306bfb75d01e5567d4e7391ad5075
jbbarth <jeanbaptiste.barth@gmail.com>
2011-04-20 11:32:08 +0200

    Avoid errors when parsing date in migrations from legacy database

--- db/migrate/20110406100558_convert_dates_from_string_to_dates.rb
+++ db/migrate/20110406100558_convert_dates_from_string_to_dates.rb
@@ -5,7 +5,7 @@ class ConvertDatesFromStringToDates < ActiveRecord::Migration
         value = m.send(old_field)
         unless value.blank?
           value = "#{$1}/20#{$2}" if value.match %r{(.*\d\d)/(\d\d)$}
-          m.send("#{new_field}=", Date.parse(value))
+          m.send("#{new_field}=", (Date.parse(value) rescue nil))
           m.save
         end
       end
