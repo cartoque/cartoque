0d1fbd87cd975cc05bd0db1870f4da2c18e2320c
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-03 12:50:48 +0200

    Refactor: moved stats into partials, and created an helper to DRY view a bit

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,13 @@
 module WelcomeHelper
+  def welcome_box(html = "", &block)
+    content_tag :div, :class => "welcome-box-out" do
+      content_tag :div, :class => "welcome-box-in" do
+        if block_given?
+          capture(&block)
+        else
+          html
+        end
+      end
+    end
+  end
 end
