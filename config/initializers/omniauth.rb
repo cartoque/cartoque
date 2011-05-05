f883d1daa0df0cad29bd378b0e39afeceadfbc80
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-05 20:21:24 +0200

    Let's try a more secure method for OmniAuth.config.full_host: don't keep the request_uri part when the address is deduced from the environment, and rely on APP_CONFIG if nothing is found in env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,14 @@ Rails.application.config.middleware.use OmniAuth::Strategies::CAS, :cas_server =
 
 OmniAuth.config.full_host = Proc.new do |env|
   url = env["rack.session"]["omniauth.origin"] || env["omniauth.origin"] 
-  url.gsub(%r{/$}, "")
+  #if no url found, fall back to config/app_config.yml addresses
+  if url.blank?
+    url = "http://#{APP_CONFIG[:domain]}"
+  #else, parse it and remove both request_uri and query_string
+  else
+    uri = URI.parse(url)
+    url = "#{uri.scheme}://#{uri.host}"
+    url << ":#{uri.port}" unless uri.default_port == uri.port
+  end
+  url
 end
