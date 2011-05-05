656a0c73aeb0a560e4d29d18051bebbe330c733b
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-05 18:37:08 +0200

    Configure OmniAuth.config.full_host so that the application can work behind a reverse proxy (we're redirected to request.url otherwise, which is an internal domain on a private port)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,8 @@ require 'omniauth/enterprise'
 
 cas_server = Rails.env == 'production' ? 'https://authentification-cerbere.application.i2/cas/' : 'http://localhost:9292'
 Rails.application.config.middleware.use OmniAuth::Strategies::CAS, :cas_server => cas_server
+
+OmniAuth.config.full_host = Proc.new do |env|
+  url = env["rack.session"]["omniauth.origin"] || env["omniauth.origin"] 
+  url.gsub(%r{/$})
+end
