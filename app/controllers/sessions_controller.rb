64c102a1d0dd2ecbaea88a4d70d041f05a1c4d49
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-05 18:34:36 +0200

    Fixed omniauth failed login sometimes redirect to ?message=invalid_user/auth/cas, so we should strip the last part

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def failure
-    flash[:error] = t(:"omniauth_#{params[:message]}")
+    flash[:error] = t(:"omniauth_#{params[:message].gsub(%r(/.*),"")}")
   end
 
   def destroy
