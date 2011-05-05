fe8af1318aff480eb3ae750d887d1d56f2ce68ea
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-05 13:50:35 +0200

    Fixed cerbere url for CAS authentication in production mode

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 require 'omniauth/enterprise'
 
-cas_server = Rails.env == 'production' ? 'https://authentification-cerbere.application.i2' : 'http://localhost:9292'
+cas_server = Rails.env == 'production' ? 'https://authentification-cerbere.application.i2/cas/' : 'http://localhost:9292'
 Rails.application.config.middleware.use OmniAuth::Strategies::CAS, :cas_server => cas_server
