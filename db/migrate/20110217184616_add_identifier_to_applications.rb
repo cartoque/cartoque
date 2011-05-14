653a9c87ef33bb292fb3581e72a83948bd194341
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-14 15:48:54 +0200

    Removed automatic identifier update from migrations

--- db/migrate/20110217184616_add_identifier_to_applications.rb
+++ db/migrate/20110217184616_add_identifier_to_applications.rb
@@ -1,10 +1,6 @@
 class AddIdentifierToApplications < ActiveRecord::Migration
   def self.up
     add_column :applications, :identifier, :string
-    Application.all.each do |application|
-      application.identifier = application.nom.parameterize unless application.identifier
-      application.save
-    end
   end
 
   def self.down
