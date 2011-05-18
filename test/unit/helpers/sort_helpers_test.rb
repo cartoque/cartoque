b6c28b3de0253ae05c5d0174597484b6ec587c15
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-18 13:41:43 +0200

    Fixed failing tests

--- test/unit/helpers/sort_helpers_test.rb
+++ test/unit/helpers/sort_helpers_test.rb
@@ -8,6 +8,10 @@ class FakeController
   def resource_class
     Machine
   end
+
+  def direction; sort_direction; end
+  def column; sort_column; end
+  def option; sort_option; end
 end
 
 class SortHelpersTest < ActionView::TestCase
@@ -17,45 +21,45 @@ class SortHelpersTest < ActionView::TestCase
 
   test "sort_direction should default to 'name'" do
     @controller.params = {}
-    assert_equal "name", @controller.sort_column
+    assert_equal "name", @controller.column
   end
 
   test "sort_option should only accept attributes among model column_names" do
     @controller.params = {:sort => "manufacturer"}
-    assert_equal "manufacturer", @controller.sort_column
+    assert_equal "manufacturer", @controller.column
     @controller.params = {:sort => "namez"}
-    assert_equal "name", @controller.sort_column
+    assert_equal "name", @controller.column
     @controller.params = {:sort => ["test"]}
-    assert_equal "name", @controller.sort_column
+    assert_equal "name", @controller.column
   end
 
   test "sort_option should accept coma separated values" do
     @controller.params = {:sort => "name,manufacturer"}
-    assert_equal "name,manufacturer", @controller.sort_column
+    assert_equal "name,manufacturer", @controller.column
     @controller.params = {:sort => "name,manufacturer,not_a_column"}
-    assert_equal "name,manufacturer", @controller.sort_column
+    assert_equal "name,manufacturer", @controller.column
   end
 
   test "sort_direction should default to 'asc'" do
     @controller.params = {}
-    assert_equal "asc", @controller.sort_direction
+    assert_equal "asc", @controller.direction
   end
 
   test "sort_direction should only accept values among asc/desc" do
     @controller.params = {:direction => "desc"}
-    assert_equal "desc", @controller.sort_direction
+    assert_equal "desc", @controller.direction
     @controller.params = {:direction => "asc"}
-    assert_equal "asc", @controller.sort_direction
+    assert_equal "asc", @controller.direction
     @controller.params = {:direction => "blah"}
-    assert_equal "asc", @controller.sort_direction
+    assert_equal "asc", @controller.direction
     @controller.params = {:direction => ["test","blah"]}
-    assert_equal "asc", @controller.sort_direction
+    assert_equal "asc", @controller.direction
   end
 
   test "sort_option should be a correct mix between sort_column and sort_direction" do
     @controller.params = {}
-    assert_equal "name asc", @controller.sort_option
+    assert_equal "name asc", @controller.option
     @controller.params = {:sort => "name,manufacturer", :direction => "desc"}
-    assert_equal "name desc, manufacturer desc", @controller.sort_option
+    assert_equal "name desc, manufacturer desc", @controller.option
   end
 end
