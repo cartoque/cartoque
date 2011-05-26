a6ae5d21af7d687c8eef540306e880cf55e9942d
jbbarth <jeanbaptiste.barth@gmail.com>
2011-05-26 17:22:59 +0200

    Introduced 960.gs grid system + many style updates

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,7 +1,7 @@
 module WelcomeHelper
   def welcome_box(html = "", &block)
-    content_tag :div, :class => "welcome-box-out" do
-      content_tag :div, :class => "welcome-box-in" do
+    content_tag :div, :class => "grid_3" do
+      content_tag :div, :class => "welcome-box" do
         if block_given?
           capture(&block)
         else
