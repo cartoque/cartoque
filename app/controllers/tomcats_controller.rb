ff1a17a02d966ba63dcdfd01312c99624ed1aa67
jbbarth <jeanbaptiste.barth@gmail.com>
2011-06-15 08:56:20 +0200

    Added filter by server for tomcats/index

--- app/controllers/tomcats_controller.rb
+++ app/controllers/tomcats_controller.rb
@@ -1,5 +1,16 @@
 class TomcatsController < ApplicationController
+  respond_to :html, :js
+
   def index
     @tomcats = Tomcat.all
+    @filters = {}
+    @tomcats.each do |tomcat|
+      tomcat.each do |key,value|
+        @filters[key] ||= []
+        @filters[key] << value unless @filters[key].include?(value)
+        @filters[key] = @filters[key].compact.sort
+      end
+    end
+    @tomcats = @tomcats.select{|t| t[:server] == params[:by_server]} if params[:by_server].present?
   end
 end
