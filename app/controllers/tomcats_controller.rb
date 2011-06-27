1b7f96ea05088cb5894ad9ed262bac284260054a
jbbarth <jeanbaptiste.barth@gmail.com>
2011-06-27 19:21:08 +0200

    Added ability to filter tomcats by vip

--- app/controllers/tomcats_controller.rb
+++ app/controllers/tomcats_controller.rb
@@ -9,11 +9,13 @@ class TomcatsController < ApplicationController
       tomcat.each do |key,value|
         @filters[key] ||= []
         value = value.split("_").first if key == :tomcat
+        value = value.sub(/:80$/, "") if key == :vip
         @filters[key] << value unless @filters[key].include?(value)
         @filters[key] = @filters[key].compact.sort
       end
     end
     #restrict tomcats
+    @tomcats = @tomcats.select{|t| t[:vip].sub(/:80$/,"") == params[:by_vip]} if params[:by_vip].present?
     @tomcats = @tomcats.select{|t| t[:server] == params[:by_server]} if params[:by_server].present?
     @tomcats = @tomcats.select{|t| t[:tomcat].starts_with?(params[:by_tomcat]) } if params[:by_tomcat].present?
     @tomcats = @tomcats.select{|t| t[:java_version].starts_with?(params[:by_java]) } if params[:by_java].present?
