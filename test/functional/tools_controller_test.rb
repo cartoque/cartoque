55ac788ee5bb42c1cc39ae2dce794a368b5e74f4
jbbarth <jeanbaptiste.barth@gmail.com>
2011-06-28 15:05:08 +0200

    Fixed broken test (no more route on 'tools#show' without an :id)

--- test/functional/tools_controller_test.rb
+++ test/functional/tools_controller_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 class ToolsControllerTest < ActionController::TestCase
   test "should return a 404 if no tool given" do
-    get :show
-    assert_response 404
     get :show, :id => :non_existent
     assert_response 404
     assert @response.body.include?("These are not the droids you're looking for")
