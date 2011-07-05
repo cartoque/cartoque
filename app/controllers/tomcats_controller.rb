899dbf08126eaa95c4f8e7eb8a65583f9a1ecd3d
jbbarth <jeanbaptiste.barth@gmail.com>
2011-07-05 07:30:32 +0200

    Added some filters to old tomcats view

--- app/controllers/tomcats_controller.rb
+++ app/controllers/tomcats_controller.rb
@@ -22,5 +22,12 @@ class TomcatsController < ApplicationController
 
   def index_old
     @tomcats_old = TomcatOld.all
+    #filters options
+    @filters = {}
+    @filters[:tomcat] = %w(tomcat4 tomcat5)
+    @filters[:server] = @tomcats_old.map{|t| t[:server]}.compact.sort.uniq
+    #restrict tomcats
+    @tomcats_old = @tomcats_old.select{|t| t[:server] == params[:by_server]} if params[:by_server].present?
+    @tomcats_old = @tomcats_old.select{|t| t[:tomcat].starts_with?(params[:by_tomcat]) } if params[:by_tomcat].present?
   end
 end
