e06815e5883b69a3b904d702f5fc9fa279e0ec89
jbbarth <jeanbaptiste.barth@gmail.com>
2011-07-13 11:51:14 +0200

    Fixed unability to update ApplicationInstance timestamps from nested forms

--- app/models/application_instance.rb
+++ app/models/application_instance.rb
@@ -6,7 +6,8 @@ class ApplicationInstance < ActiveRecord::Base
   accepts_nested_attributes_for :application_urls, :reject_if => lambda{|a| a[:url].blank? },
                                                    :allow_destroy => true
 
-  attr_accessible :name, :application_id, :machine_ids, :authentication_method, :application_urls_attributes
+  attr_accessible :name, :application_id, :machine_ids, :authentication_method, :application_urls_attributes,
+                  :created_at, :updated_at
 
   AVAILABLE_AUTHENTICATION_METHODS = %w(none cerbere cerbere-cas cerbere-bouchon ldap-minequip internal other)
 
