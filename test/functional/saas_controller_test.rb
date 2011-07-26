d51da1f99f5f0ebe9903012e3bc952573c748ce4
jbbarth <jeanbaptiste.barth@gmail.com>
2011-07-26 20:07:32 +0200

    Removed obsolete functional test

--- test/functional/saas_controller_test.rb
+++ test/functional/saas_controller_test.rb
@@ -1,14 +1,8 @@
 require 'test_helper'
 
 class SaasControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
   test "should get show" do
     get :show, :id => :redmine
     assert_response :success
   end
-
 end
