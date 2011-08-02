36e513669f7a7224f5d5bc60dd707520497d5a94
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-02 19:00:26 +0200

    Hide loading box with CSS so that it doesn't show up at first on loong pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,6 @@
 // ajax loader
 $(function() {
-  $('#loading').hide()
-               .ajaxStart(function() { $(this).show(); })
+  $('#loading').ajaxStart(function() { $(this).show(); })
                .ajaxStop(function() { $(this).hide(); });
 });
 
