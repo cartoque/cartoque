21df88830c645fc78a76d363408496808f9fd3eb
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-11 08:22:18 +0200

    Update Factories declarations so that it doesn't break each time we update the database and want to db:migrate/db:schema:load the test environment

--- test/factories/database.rb
+++ test/factories/database.rb
@@ -1,11 +1,11 @@
 Factory.define :database do |s|
   s.name "database-01"
   s.database_type "postgres"
-  s.machine_ids [Factory(:machine).id]
+  s.machine_ids { [Factory(:machine).id] }
 end
 
 Factory.define :oracle, :parent => :database do |s|
   s.name "database-02"
   s.database_type "oracle"
-  s.machine_ids [Factory(:virtual).id]
+  s.machine_ids { [Factory(:virtual).id] }
 end
--- test/factories/storage.rb
+++ test/factories/storage.rb
@@ -1,4 +1,4 @@
 Factory.define :storage do |s|
-  s.machine_id Factory(:machine).id
+  s.machine_id { Factory(:machine).id }
   s.constructor "IBM"
 end
