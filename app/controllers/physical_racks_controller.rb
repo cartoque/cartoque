f6b60c1069e4553ef27cc2173333af38ef78a2ab
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-21 13:42:05 +0200

    Improved physical_racks/index view with servers count

--- app/controllers/physical_racks_controller.rb
+++ app/controllers/physical_racks_controller.rb
@@ -1,11 +1,18 @@
 class PhysicalRacksController < InheritedResources::Base
   layout "admin"
 
+  before_filter :count_machines_per_rack, :only => :index
+
   def create
     create! { physical_racks_url }
   end
 
   def update
     update! { physical_racks_url }
+  end
+
+  protected
+  def count_machines_per_rack
+    @machines_count = Machine.where("virtual = ?", false).group("physical_rack_id").count
   end
 end
