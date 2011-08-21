12516bf3ef271546e6f3d3f4d37a471bac321b52
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-21 13:36:10 +0200

    Improved sites/index view

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,11 +1,19 @@
 class SitesController < InheritedResources::Base
   layout "admin"
 
+  before_filter :count_racks_per_site, :only => :index
+
   def create
     create! { sites_url }
   end
 
   def update
     update! { sites_url }
+  end
+
+  protected
+  def count_racks_per_site
+    @racks_count = PhysicalRack.group("site_id").count
+    @machines_count = Machine.where("virtual = ?", false).group("physical_racks.site_id").count
   end
 end
