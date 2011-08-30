5c755add07093c1e7d167ecd0dfa83385ea3cd9a
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-30 13:19:56 +0200

    Added ability to click on a sector and see the repartition of sub-operating systems

--- app/controllers/operating_systems_controller.rb
+++ app/controllers/operating_systems_controller.rb
@@ -5,6 +5,12 @@ class OperatingSystemsController < InheritedResources::Base
     @operating_systems = OperatingSystem.arrange(:order => "name")
     @os_servers_count = Server.group("operating_system_id").count
     @os_servers_count.default = 0
+    if params[:graph_subtree]
+      @root_os = OperatingSystem.find(params[:graph_subtree].to_i)
+      @os_subtree = @root_os.descendants.arrange(:order => :name) if @root_os.present?
+    else
+      @os_subtree = @operating_systems
+    end
   end
 
   def create
