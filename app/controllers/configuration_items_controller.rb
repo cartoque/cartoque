239cf510f431e00294e53fdfec0c896bbac6731f
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-31 14:13:10 +0200

    Added standard filters goodness to configuration_items/index

--- app/controllers/configuration_items_controller.rb
+++ app/controllers/configuration_items_controller.rb
@@ -1,3 +1,7 @@
 class ConfigurationItemsController < InheritedResources::Base
   actions :index, :show
+  
+  respond_to :html, :js
+
+  has_scope :by_item_type
 end
--- app/models/configuration_item.rb
+++ app/models/configuration_item.rb
@@ -5,9 +5,17 @@ class ConfigurationItem < ActiveRecord::Base
 
   before_validation :update_identifier!
 
+  scope :by_item_type, proc {|type| where(:item_type => type) }
+
   def update_identifier!
     if self.item.present?
       self.identifier = "#{self.item_type.downcase}::#{self.item.to_s.try(:parameterize)}"
+    end
+  end
+
+  def self.real_object_classes
+    @@real_object_classes ||= ActiveRecord::Base.subclasses.select do |klass|
+      klass != ConfigurationItem && klass.instance_methods.include?(:configuration_item)
     end
   end
 end
