521a28bd38f082c980e7f057d22d7e75cccb3ac6
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-31 12:00:06 +0200

    Added ConfigurationItem#identifier generation

--- app/models/configuration_item.rb
+++ app/models/configuration_item.rb
@@ -2,4 +2,12 @@ class ConfigurationItem < ActiveRecord::Base
   belongs_to :item, :polymorphic => true
 
   validates_presence_of :item
+
+  before_validation :update_identifier!
+
+  def update_identifier!
+    if self.item.present?
+      self.identifier = "#{self.item_type.downcase}::#{self.item.to_s.parameterize}"
+    end
+  end
 end
--- test/unit/configuration_item_test.rb
+++ test/unit/configuration_item_test.rb
@@ -5,4 +5,18 @@ class ConfigurationItemTest < ActiveSupport::TestCase
     assert ! ConfigurationItem.new.valid?
     assert ConfigurationItem.new(:item => Factory(:server)).valid?
   end
+
+  context "#identifier" do
+    should "not raise if there's no item (other validations will fail in this case)" do
+      ci = ConfigurationItem.new
+      assert ! ci.valid?
+    end
+
+    should "generate the identifier before validations" do
+      ci = ConfigurationItem.new(:item => Factory(:server))
+      assert ci.valid?
+      assert_not_nil ci.identifier
+      assert ci.save
+    end
+  end
 end
