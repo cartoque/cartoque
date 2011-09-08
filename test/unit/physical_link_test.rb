e53af4735af66e686067283d69da111c2abe6321
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-08 19:34:28 +0200

    Fixed failing PhysicalLink unit test

--- test/unit/physical_link_test.rb
+++ test/unit/physical_link_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class PhysicalLinkTest < ActiveSupport::TestCase
   should "be able to add a physical link to a server" do
     from, to = Factory(:server), Factory(:virtual)
-    to.physical_links = [ PhysicalLink.new(:switch => to) ]
     assert_difference "PhysicalLink.count", +1 do
-      assert to.save
+      from.physical_links = [ PhysicalLink.new(:link_type => "eth", :switch => to) ]
+      from.save
     end
   end
 end
