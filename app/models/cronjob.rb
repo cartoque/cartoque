5cf2ba60b72fd7c35daab59798829e0368722763
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-16 08:53:57 +0200

    Added Cronjob#parse_line for future use in importer

--- app/models/cronjob.rb
+++ app/models/cronjob.rb
@@ -1,7 +1,21 @@
 class Cronjob < ActiveRecord::Base
   belongs_to :server
 
+  validates_presence_of :name, :server, :frequency, :command
+
   scope :by_server, proc { |server_id| where(:server_id => server_id) }
   scope :by_name, proc { |search| where("cronjobs.name LIKE ?", "%#{search}%") }
   scope :by_command, proc { |search| where("cronjobs.command LIKE ?", "%#{search}%") }
+
+  def self.parse_line(line)
+    #typical line: "0 6 * * * root /usr/bin/my_script.sh >/dev/null && blah
+    cron = Cronjob.new
+    elems = line.strip.split(/\s+/)
+    if elems.size >= 6 && !elems.first.match(/^\s*[#a-z]/)
+      cron.frequency = elems[0..4].join(" ")
+      cron.user = elems[5]
+      cron.command = elems[6..-1].join(" ")
+    end
+    cron
+  end
 end
--- test/unit/cronjob_test.rb
+++ test/unit/cronjob_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class CronjobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "#parse" do
+    should "parse a simple, standard cron line" do
+      line = "00  05  *  *  *  root  /opt/scripts/my-own-script"
+      cron = Cronjob.parse_line(line)
+      assert !cron.valid?
+      cron.name = "name"
+      assert !cron.valid?
+      cron.server_id = Factory(:server).id
+      assert cron.valid?
+      assert_equal "00 05 * * *", cron.frequency
+      assert_equal "root", cron.user
+      assert_equal "/opt/scripts/my-own-script", cron.command
+    end
+
+    should "not be valid with 'strongly' invalid cron lines" do
+      [ "",
+        "      ",
+        "less than six elements in line",
+        "m h d  m  w  user  command",
+        "#* * * * * user commented cron"
+      ].each do |line|
+        assert !Cronjob.parse_line(line).valid?
+      end
+    end
+
+    should "be able to parse a cron file correctly" do
+      server = Server.find_or_create_by_name("server-01")
+      crons = File.readlines(File.expand_path("../../data/crons/server-01.cron", __FILE__)).map do |line|
+        c = Cronjob.parse_line(line)
+        c.server = server
+        c.name = "unknown"
+        c
+      end
+      assert ! crons.shift.valid?
+      assert_equal 13, crons.size
+      assert_equal [true], crons.map(&:valid?).uniq
+    end
+  end
 end
