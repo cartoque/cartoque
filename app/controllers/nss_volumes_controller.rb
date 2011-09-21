8f1b57c138c6a9af32e7449cb09b776560fb31d4
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-21 20:05:18 +0200

    Add NssVolume#by_client scope and associated filter in nss_volumes/index

--- app/controllers/nss_volumes_controller.rb
+++ app/controllers/nss_volumes_controller.rb
@@ -2,10 +2,11 @@ class NssVolumesController < InheritedResources::Base
   respond_to :html, :js
   actions :index, :show
 
-  before_filter :find_servers
+  before_filter :find_servers_and_clients
 
   has_scope :by_server
   has_scope :by_name
+  has_scope :by_client
   has_scope :by_type
   has_scope :by_guid
   has_scope :by_snapshot_status
@@ -15,7 +16,8 @@ class NssVolumesController < InheritedResources::Base
     @nss_volumes ||= end_of_association_chain.joins(:server).includes(:clients).order("servers.name asc, nss_volumes.name asc")
   end
 
-  def find_servers
+  def find_servers_and_clients
     @servers = Server.where(:id => NssVolume.select("distinct(server_id)").map(&:server_id)).order("name asc")
+    @clients = Server.where(:id => NssAssociation.select("distinct(server_id)").map(&:server_id)).order("name asc")
   end
 end
--- app/models/nss_volume.rb
+++ app/models/nss_volume.rb
@@ -5,6 +5,7 @@ class NssVolume < ActiveRecord::Base
 
   scope :by_server, proc { |server_id| where(:server_id => server_id) }
   scope :by_name, proc { |search| where("nss_volumes.name LIKE ?", "%#{search}%") }
+  scope :by_client, proc { |client_id| joins(:clients).where("nss_associations.server_id" => client_id) }
   scope :by_type, proc { |type| where(:falconstor_type => type) }
   scope :by_guid, proc { |search| where("guid LIKE ?", "%#{search}%") }
   scope :by_snapshot_status, proc { |status| where(:snapshot_enabled => status) }
