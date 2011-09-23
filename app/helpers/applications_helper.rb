284b6c865abcac67586b5e1b7a889d5667f747cc
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-23 10:52:41 +0200

    Removed no more used ApplicationHelper#cerbere_image

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationsHelper
-  def cerbere_image(has_cerbere, ifnot = false)
-    if has_cerbere
-      image_tag("login.png", :size => "16x16")
-    elsif ifnot
-      image_tag("blank.gif")
-    end
-  end
-
   def collection_for_authentication_methods
     ApplicationInstance::AVAILABLE_AUTHENTICATION_METHODS.map do |meth|
       [ t("authentication.#{meth}"), meth ]
