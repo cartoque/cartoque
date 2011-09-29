9fbf511397de382b82d28bdbb471a065229a423c
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-29 07:39:42 +0200

    Moved tools/cluster_symetry logic where it should live, in the controller (argl!)

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,2 +1,28 @@
+require 'find'
+
 class ToolsController < ApplicationController
+  def cluster_symetry
+    path = Rails.root.join("data/symetry")
+    @clusters = {}
+    Dir.glob("#{path}/*").map{|c| File.basename(c)}.sort.each do |cluster|
+      @clusters[cluster] = { :path => "#{path}/#{cluster}",
+                            :nodes => Dir.glob("#{path}/#{cluster}/*").map{|c| File.basename(c)}.sort }
+      files = []
+      lists = []
+      Find.find(@clusters[cluster][:path]).each do |file|
+        if file.ends_with?(".list")
+          lists << file.sub(@clusters[cluster][:path],"").sub(%r{^/[^/]+},"")
+        elsif File.file?(file)
+          files << file.sub(@clusters[cluster][:path],"").sub(%r{^/[^/]+},"")
+        end
+      end
+      @clusters[cluster][:files] = files.sort.uniq
+      @clusters[cluster][:lists] = lists.reject{|f| f.ends_with?("packages.list")}.sort.uniq
+    end
+    @cluster_names = @clusters.keys
+    #limit clusters
+    @clusters.reject! do |cluster,hsh|
+      cluster != params[:id]
+    end unless params[:id] == "all"
+  end
 end
