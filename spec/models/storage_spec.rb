24d64fee2813a21db88d0a15b5da5d9f60765f12
jbbarth <jeanbaptiste.barth@gmail.com>
2011-10-14 14:40:39 +0200

    Completed storage spec

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Storage do
-  pending "should be valid with just a name and a server"
+  it "should be valid with just a constructor and a server" do
+    storage = Storage.new
+    storage.should_not be_valid
+    storage.should have(2).errors
+    storage.server = Factory(:server)
+    storage.constructor = "IBM"
+    storage.should be_valid
+  end
 end
