d5686ae6d0c0799ed071f719a865d399049fbc4b
jbbarth <jeanbaptiste.barth@gmail.com>
2011-10-19 13:53:40 +0200

    Added filters in licenses/index

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,4 +1,17 @@
 class LicensesController < InheritedResources::Base
   respond_to :html, :js
   actions :index
+
+  has_scope :by_editor
+  has_scope :by_key
+  has_scope :by_title
+  has_scope :by_server
+
+  before_filter :find_filter_keys, :only => :index
+
+  private
+  def find_filter_keys
+    @editors = License.select("distinct(editor)").map(&:editor).sort
+    @servers = Server.where(:id => License.joins(:servers).select("distinct(servers.id)").map(&:id)).sort_by(&:name)
+  end
 end
