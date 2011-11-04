562c376c723427be40d705a08ca4a6609ef43b82
jbbarth <jeanbaptiste.barth@gmail.com>
2011-11-04 13:20:26 +0100

    Change renewal date color depending on the distance from today in licenses/index

--- app/helpers/licenses_helper.rb
+++ app/helpers/licenses_helper.rb
@@ -1,2 +1,13 @@
 module LicensesHelper
+  def renewal_limit(date)
+    return "" if date.blank?
+    months_before_end = ((date - Date.today) / 30).to_f
+    if months_before_end <= 6
+      content_tag(:span, l(date), :class => "renewal-critical")
+    elsif months_before_end <= 12
+      content_tag(:span, l(date), :class => "renewal-warning")
+    else
+      l(date)
+    end
+  end
 end
