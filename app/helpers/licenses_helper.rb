5dcfc2455d4743c8971036cf0e2e453e0786d480
jbbarth <jeanbaptiste.barth@gmail.com>
2011-11-04 13:33:26 +0100

    Softer renewal date limits for licenses

--- app/helpers/licenses_helper.rb
+++ app/helpers/licenses_helper.rb
@@ -1,10 +1,10 @@
 module LicensesHelper
   def renewal_limit(date)
     return "" if date.blank?
-    months_before_end = ((date - Date.today) / 30).to_f
-    if months_before_end <= 6
+    days_before_end = date - Date.today
+    if days_before_end <= 15
       content_tag(:span, l(date), :class => "renewal-critical")
-    elsif months_before_end <= 12
+    elsif days_before_end <= 3*30
       content_tag(:span, l(date), :class => "renewal-warning")
     else
       l(date)
