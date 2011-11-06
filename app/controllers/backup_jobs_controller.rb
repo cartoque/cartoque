4d7ed0c48e00a8b868fe6991bd512c6af79ce151
jbbarth <jeanbaptiste.barth@gmail.com>
2011-11-06 19:30:28 +0100

    Allow filtering by server in backup_jobs/index

--- app/controllers/backup_jobs_controller.rb
+++ app/controllers/backup_jobs_controller.rb
@@ -1,4 +1,17 @@
 class BackupJobsController < InheritedResources::Base
   respond_to :html, :js
   actions :index
+
+  has_scope :by_server
+
+  before_filter :find_filters, :only => :index
+  
+  protected
+  def collection
+    @backup_jobs ||= end_of_association_chain.includes(:server).where("servers.status" => Server::STATUS_ACTIVE).order("servers.name asc")
+  end
+
+  def find_filters
+    @servers = Server.find( BackupJob.select("distinct(server_id)").map(&:server_id) ).select(&:active?).sort_by(&:name)
+  end
 end
--- app/models/backup_job.rb
+++ app/models/backup_job.rb
@@ -1,5 +1,7 @@
 class BackupJob < ActiveRecord::Base
   belongs_to :server
 
+  scope :by_server, proc{ |server_id| where(:server_id => server_id) }
+
   validates_presence_of :server, :hierarchy
 end
