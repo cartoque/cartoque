96dee6ae8bc16ac541e3fed098eb00b3072fe7ad
jbbarth <jeanbaptiste.barth@gmail.com>
2011-11-28 18:01:59 +0100

    Limit the number of contacts/companies displayed in contacts/index

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,18 @@
 class ContactsController < InheritedResources::Base
+  before_filter :find_companies_and_contacts_count, :only => :index
+
   def index
     @companies = Company.all
     index!
+  end
+
+  private
+  def collection
+    @contacts ||= end_of_association_chain.limit(10).order("updated_at desc")
+  end
+
+  def find_companies_and_contacts_count
+    @companies_count = Company.count
+    @contacts_count  = Contact.count
   end
 end
