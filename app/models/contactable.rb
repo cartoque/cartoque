748f8b39d23d83e9dd9764e0402662170dbd927a
jbbarth <jeanbaptiste.barth@gmail.com>
2011-12-05 17:36:58 +0100

    Added website_infos to Company

--- app/models/contactable.rb
+++ app/models/contactable.rb
@@ -6,6 +6,8 @@ module Contactable
       accepts_nested_attributes_for :email_infos, :reject_if => lambda{|a| a[:value].blank? }, :allow_destroy => true
       has_many :phone_infos, :class_name => "ContactInfo", :conditions => {:info_type => "phone"}, :as => :entity
       accepts_nested_attributes_for :phone_infos, :reject_if => lambda{|a| a[:value].blank? }, :allow_destroy => true
+      has_many :website_infos, :class_name => "ContactInfo", :conditions => {:info_type => "website"}, :as => :entity
+      accepts_nested_attributes_for :website_infos, :reject_if => lambda{|a| a[:value].blank? }, :allow_destroy => true
     end
   end
 
