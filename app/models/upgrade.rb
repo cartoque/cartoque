2347df215e43d3e68bf30750ba93da12b8cee8de
jbbarth <jeanbaptiste.barth@gmail.com>
2011-12-14 10:53:28 +0100

    Serialize Upgrade#packages_list attribute

--- app/models/upgrade.rb
+++ app/models/upgrade.rb
@@ -1,3 +1,4 @@
 class Upgrade < ActiveRecord::Base
   belongs_to :server
+  serialize :packages_list
 end
--- spec/models/upgrade_spec.rb
+++ spec/models/upgrade_spec.rb
@@ -7,4 +7,14 @@ describe Upgrade do
     upgrade.server.should eq server
     server.upgrade.should eq upgrade
   end
+
+  it "should serialize #packages_list" do
+    server = Factory(:server)
+    upgrade = Upgrade.create!(:server_id => server.id)
+    obj = {this: ["is", "a", "test"]}
+    upgrade.packages_list = obj
+    upgrade.save
+    upgrade.reload
+    upgrade.packages_list.should eq obj
+  end
 end
