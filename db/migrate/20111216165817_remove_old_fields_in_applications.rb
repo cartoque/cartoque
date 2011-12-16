08cb5985d5c61e4ce635be1f305350df9a83e690
jbbarth <jeanbaptiste.barth@gmail.com>
2011-12-16 18:03:56 +0100

    Removed 3 old columns in applications table

---
+++ db/migrate/20111216165817_remove_old_fields_in_applications.rb
@@ -0,0 +1,13 @@
+class RemoveOldFieldsInApplications < ActiveRecord::Migration
+  def up
+    remove_column :applications, :iaw
+    remove_column :applications, :pe
+    remove_column :applications, :ams
+  end
+
+  def down
+    add_column :applications, :iaw, :string, :limit => 55, :default => "", :null => false
+    add_column :applications, :pe, :string, :limit => 55, :default => "", :null => false
+    add_column :applications, :ams, :string, :limit => 55, :default => "", :null => false
+  end
+end
