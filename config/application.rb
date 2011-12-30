4979d81ea68ad2754412dc1cc545500366ca6717
jbbarth <jeanbaptiste.barth@gmail.com>
2011-12-30 11:37:06 +0100

    Rails does not fallback on default locale on some circumstances, so I hardcode locale='fr' until we have a clean way to manage multiple locales

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Cartoque
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :fr
+    config.i18n.locale = :fr
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
