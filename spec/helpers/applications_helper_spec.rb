dbdcef59adade5f98c3c5c021d2786a23b129033
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-16 00:10:38 +0100

    Fixed helper test broken by introduction of :en locale

--- spec/helpers/applications_helper_spec.rb
+++ spec/helpers/applications_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ApplicationsHelper do
   it "#collection_for_authentication_methods" do
     select = collection_for_authentication_methods
-    assert select.include?(["Interne", "internal"])
+    assert select.include?([I18n.t(:internal), "internal"])
     assert_equal ApplicationInstance::AVAILABLE_AUTHENTICATION_METHODS.size, select.size
   end
 end
