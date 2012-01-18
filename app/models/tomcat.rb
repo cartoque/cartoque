81b017b9824d48a65b38ad4a457342c662696934
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-18 09:40:32 +0100

    Temporary/ugly fix for cerbere informations

--- app/models/tomcat.rb
+++ app/models/tomcat.rb
@@ -78,6 +78,7 @@ class Tomcat < Hashie::Mash
   def self.cerbere_hsh
     File.readlines( File.expand_path("../rp/cerbere.txt", self.dir) ).grep(/Protect/).inject({}) do |hsh,line|
       val = line.scan(/internal host:([^,]+), .*connection (\S+)/).first
+      next unless val
       hsh[val.first] = val.last == "ok"
       hsh
     end
