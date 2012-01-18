ab6d7a26b85f931695ed34b219f72904eeba561c
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-18 09:12:54 +0100

    boom

---
+++ assets/cartoque.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#screenshots a").fancybox({
+		'overlayShow'	: false,
+		'transitionIn'	: 'elastic',
+		'transitionOut'	: 'elastic',
+    'easingIn'      : 'easeOutBack',
+    'easingOut'     : 'easeInBack',
+		'titlePosition'	: 'over'
+	});
+});
