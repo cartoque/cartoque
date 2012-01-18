80a18e095992807e779015dd434b5ff4faad89ac
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-18 21:03:47 +0100

    Added basic API informations for servers

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,14 @@
 #some useful nanoc helpers
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+
+#ours
+require 'json'
+module Cartoque
+  module NanocHelpers
+    def json_pretty(json_string)
+      JSON.pretty_generate(JSON.parse(json_string))
+    end
+  end
+end
+include Cartoque::NanocHelpers
