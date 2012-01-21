d0b60361aceb5819c5e094450753fcf88f364d91
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-21 12:57:55 +0100

    Upgrade OmniAuth from 0.3.2 to v1.0.2
    Major benefits with this upgrade:
    - we're up-to-date and can have other OmniAuth adapters in a near future (especially omniauth-identity)
    - allowed to remove an awful hack in the initializer, our dynamic CAS config now works like any dynamic omniauth config
    - the CAS url parameter is safer than ever ; no problem anymore if the user adds a " " or a "/" at the end of the url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,22 @@
-require 'omniauth/enterprise'
+require 'omniauth/cas'
 
+# a setup app that handles dynamic config of CAS server
 setup_app = Proc.new do |env|
-  config = OmniAuth::Strategies::CAS::Configuration.new(:cas_server => Settler.safe_cas_server)
-  env['omniauth.strategy'].instance_variable_set(:@configuration, config)
+  cas_server = URI.parse(Settler.safe_cas_server) rescue nil
+  if cas_server
+    env['omniauth.strategy'].options.merge! :host => cas_server.host,
+                                            :port => cas_server.port,
+                                            :ssl  => cas_server.scheme == "https"
+  end
 end
-Rails.application.config.middleware.use OmniAuth::Strategies::CAS, :cas_server => "http://localhost:9292", :setup => setup_app
 
+# tell Rails we use this middleware, with some default value just in case
+Rails.application.config.middleware.use OmniAuth::Strategies::CAS, :host => "localhost",
+                                                                   :port => "9292",
+                                                                   :ssl => false,
+                                                                   :setup => setup_app
 
+# configures public url for our application
 OmniAuth.config.full_host = Proc.new do |env|
   url = env["rack.session"]["omniauth.origin"] || env["omniauth.origin"]
   #if no url found, fall back to config/app_config.yml addresses
@@ -19,4 +29,19 @@ OmniAuth.config.full_host = Proc.new do |env|
     url << ":#{uri.port}" unless uri.default_port == uri.port
   end
   url
+end
+
+# patch for v0.0.3
+module OmniAuth
+  module Strategies
+    class CAS
+      def return_url
+        if request.params and request.params['url']
+          {}
+        else
+          { :url => Rack::Utils.escape(request.referer) }
+        end
+      end
+    end
+  end
 end
