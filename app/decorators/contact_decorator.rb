4d36b8bbb5ae20aecdb45125032e91aaf1bd0d26
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-23 01:33:35 +0100

    Better form for contacts in applications/form

--- app/decorators/contact_decorator.rb
+++ app/decorators/contact_decorator.rb
@@ -1,11 +1,16 @@
 class ContactDecorator < ResourceDecorator
   decorates :contact
 
-  def mailing_list_form
+  def long_form
     html = "".html_safe
     html << model.last_name
     html << ", #{model.first_name}" if model.first_name.present?
     html << " (#{model.company})" if model.company.present?
+    html
+  end
+
+  def mailing_list_form
+    html = long_form
     html << " <#{model.email_infos.first}>"
     html
   end
--- spec/decorators/contact_decorator_spec.rb
+++ spec/decorators/contact_decorator_spec.rb
@@ -5,7 +5,17 @@ describe ContactDecorator do
     @contact = Factory(:contact).decorate
   end
 
+  it "return a long form with last name, first name and company" do
+    @contact.decorate.long_form.should == "Doe, John (WorldCompany)"
+    @contact.company = nil
+    @contact.decorate.long_form.should == "Doe, John"
+    @contact.first_name = nil
+    @contact.decorate.long_form.should == "Doe"
+  end
+
   it "return a clean form for mailing lists" do
-###    @contact.for_mailing_lists
+    @contact.decorate.mailing_list_form.should == "Doe, John (WorldCompany) &lt;&gt;"
+    @contact.email_infos << ContactInfo.new(info_type: "email", value: "jdoe@example.net")
+    @contact.decorate.mailing_list_form.should == "Doe, John (WorldCompany) &lt;jdoe@example.net&gt;"
   end
 end
