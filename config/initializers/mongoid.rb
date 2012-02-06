f5a8d6ebc40ed39e8631aa7a45c5ef90f6538583
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-07 00:23:34 +0100

    Added an initializer for mongoid to handle better keys

---
+++ config/initializers/mongoid.rb
@@ -0,0 +1,14 @@
+# See: http://blog.noizeramp.com/2010/12/21/friendly-composite-keys-in-mongoid/
+# String#identify is used when the `key` macro is defined in a model
+
+# Instead of the standard composition, convert everything
+# non-alpha and non-digit to dash and squeeze
+class String
+  def identify
+    if Mongoid.parameterize_keys
+      gsub(/[^a-z0-9]+/, ' ').strip.gsub(' ', '-')
+    else
+      self
+    end
+  end
+end
