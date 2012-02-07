5941e62c55c6d6d76937fe5b2a4876a1cbf9e781
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-07 08:56:24 +0100

    Removed obsolete patch for omniauth-cas (the bug is fixed in 0.0.4)

--- lib/omniauth/patches.rb
+++ lib/omniauth/patches.rb
@@ -1,20 +1,5 @@
 require 'omniauth/cas'
 
-# patch for v0.0.3
-module OmniAuth
-  module Strategies
-    class CAS
-      def return_url
-        if request.params and request.params['url']
-          {}
-        else
-          { :url => Rack::Utils.escape(request.referer) }
-        end
-      end
-    end
-  end
-end
-
 # patch to accept path (subdir) in cas_host
 module OmniAuth
   module Strategies
