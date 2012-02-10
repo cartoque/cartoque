b98029a5490a80b5ccec5544f44ad103aa7602c1
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-10 08:50:52 +0100

    Removed unused before_filter in BackupJobsController

--- app/controllers/backup_jobs_controller.rb
+++ app/controllers/backup_jobs_controller.rb
@@ -5,16 +5,11 @@ class BackupJobsController < InheritedResources::Base
   has_scope :by_server
   has_scope :by_client_type
 
-  before_filter :find_filters, :only => :index
   before_filter :find_not_backuped, :only => :index
   
   protected
   def collection
     @backup_jobs ||= end_of_association_chain.includes(:server).where("servers.status" => Server::STATUS_ACTIVE).order("servers.name asc")
-  end
-
-  def find_filters
-    @servers = Server.find( BackupJob.select("distinct(server_id)").map(&:server_id) ).select(&:active?).sort_by(&:name)
   end
 
   def find_not_backuped
