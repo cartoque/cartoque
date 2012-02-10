071a99acb0e9d8d3ab86dd367f38740f06318639
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-10 08:48:58 +0100

    Transformed server filter in backup_jobs/index, now a search field instead of a select menu

--- app/models/backup_job.rb
+++ app/models/backup_job.rb
@@ -1,7 +1,7 @@
 class BackupJob < ActiveRecord::Base
   belongs_to :server
 
-  scope :by_server, proc{ |server_id| where(:server_id => server_id) }
+  scope :by_server, proc{ |term| joins(:server).where("servers.name like ?", "%#{term}%") }
   scope :by_client_type, proc{ |client_type| where(:client_type => client_type) }
 
   validates_presence_of :server, :hierarchy
