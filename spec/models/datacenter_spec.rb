19860295e549957b183d7158322dfa042069f17a
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-20 07:19:45 +0100

    Migrate Datacenters to mongoid

--- spec/models/datacenter_spec.rb
+++ spec/models/datacenter_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe Datacenter do
+  it "has a name" do
+    Datacenter.new.should_not be_valid
+    Datacenter.new(name: "Blah").should be_valid
+  end
+
+  it "has a unique name across the instance" do
+    Datacenter.create!(name: "Datacenter")
+    dc = Datacenter.new(name: "Datacenter")
+    dc.should_not be_valid
+    dc.errors.keys.should == [:name]
+  end
+
   describe "#default" do
     it "returns first datacenter if any" do
       Datacenter.count.should == 0
-      Datacenter.create!(:name => "Hosterz")
+      Datacenter.create!(name: "Hosterz")
       Datacenter.default.name.should == "Hosterz"
     end
 
