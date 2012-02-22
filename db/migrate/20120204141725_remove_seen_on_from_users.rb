a68c0bbbfb1b09c9f7383d2a0b09975e32b7f964
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-22 23:50:08 +0100

    En-safe old migration

--- db/migrate/20120204141725_remove_seen_on_from_users.rb
+++ db/migrate/20120204141725_remove_seen_on_from_users.rb
@@ -1,7 +1,10 @@
 class RemoveSeenOnFromUsers < ActiveRecord::Migration
   def up
-    User.where("last_sign_in_at IS NULL and seen_on IS NOT NULL").each do |user|
-      user.update_attribute(:last_sign_in_at, user.read_attribute(:seen_on))
+    begin
+      User.where("last_sign_in_at IS NULL and seen_on IS NOT NULL").each do |user|
+        user.update_attribute(:last_sign_in_at, user.read_attribute(:seen_on))
+      end
+    rescue
     end
     remove_column :users, :seen_on
   end
