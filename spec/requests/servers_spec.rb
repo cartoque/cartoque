ed8b1c7f63934d07f9059bb9b7a3018bc0b5cf69
jbbarth <jeanbaptiste.barth@gmail.com>
2012-02-27 01:19:07 +0100

    Fixed problems in servers/form view and in servers request spec

--- spec/requests/servers_spec.rb
+++ spec/requests/servers_spec.rb
@@ -2,24 +2,38 @@ require 'spec_helper'
 
 describe "Servers" do
   let(:user) { FactoryGirl.create(:user) }
+  let!(:server) { Server.create(name: "srv-01") }
+  let(:application) { Application.create!(name: "app-01") }
+  let(:app_instance) { ApplicationInstance.create!(name: "prod", application: application) }
 
   before do
     login_as user
-    @srv = Server.create!(name: "srv-01")
   end
 
   describe "GET /servers" do
     it "gets all servers" do
-      get servers_path
-      response.status.should be 200
-      response.body.should include "srv-01"
+      visit servers_path
+      page.status_code.should be 200
+      page.should have_content "srv-01"
     end
   end
 
   describe "GET /servers/:id" do
     it "shows a server page" do
-      get server_path(@srv)
-      response.body.should have_selector "h2", content: "Server SRV-01"
+      visit server_path(server)
+      page.should have_selector "h1", text: "Server SRV-01"
+    end
+  end
+
+  describe "GET /servers/new" do
+    it "creates a new server" do
+      visit new_server_path
+    end
+  end
+
+  describe "GET /servers/:id/edit" do
+    it "edits a server" do
+      visit edit_server_path(server)
     end
   end
 end
