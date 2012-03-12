b2e3246d8b9ebc557ba74137fc0d4b05f3acba13
jbbarth <jeanbaptiste.barth@gmail.com>
2012-03-12 17:45:10 +0100

    Fixed failing spec because Draper doesn't seem to pass #destroy method to model

--- app/decorators/resource_decorator.rb
+++ app/decorators/resource_decorator.rb
@@ -1,4 +1,8 @@
 # Common pattern for decorating resources managed with InheritedResources
 # See: https://github.com/jcasimir/draper/issues/99 for more details
 class ResourceDecorator < ApplicationDecorator
+  #TODO: understand why this method isn't passed to model
+  def destroy
+    model.destroy
+  end
 end
