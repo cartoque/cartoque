c3c83a80f1cc26b3d9280479b73516f4c6bceb87
jbbarth <jeanbaptiste.barth@gmail.com>
2012-03-21 18:40:45 +0100

    Removed attr_accessible again from Role, it was breaking model's spec

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,8 +5,6 @@ class Role
   field :name, type: String
   field :position, type: Integer, default: -> { Role.max(:position).to_i + 1 }
 
-  attr_accessible :name
-
   validates_presence_of :name
   validates_uniqueness_of :name
 
