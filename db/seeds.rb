f63a8dfad55bedcacadfb99fd1e09ea7cb65b262
jbbarth <jeanbaptiste.barth@gmail.com>
2012-03-28 12:12:11 +0200

    Remove obsolete things in db:seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,3 @@ db = ActiveRecord::Base.connection
 
 #create a first user if none
 User.create!(name: "admin", email: "admin@example.net", password: "admin") if User.count == 0
-
-#downcase server names
-#TODO: add a validation on the name
-Server.all.each do |server|
-  server.update_attribute("name", server.name.downcase)
-end
-
-#set servers identifier
-Server.where(identifier: nil).each do |m|
-  m.update_attribute(:identifier, Server.identifier_for(m.name))
-end
-
-#populate Server#network_device if needed
-if Server.network_devices.none? && PhysicalLink.any?
-  Server.find( PhysicalLink.select("distinct(switch_id)").map(&:switch_id) ).each do |switch|
-    switch.update_attribute(:network_device, true)
-  end
-end
-
-#populate CIs if needed
-ActiveRecord::Base.subclasses.select do |klass|
-  klass != ConfigurationItem && klass.instance_methods.include?(:configuration_item)
-end.each do |klass|
-  klass.all.each do |item|
-    ConfigurationItem.generate_ci_for(item) if item.configuration_item.blank?
-  end
-end
