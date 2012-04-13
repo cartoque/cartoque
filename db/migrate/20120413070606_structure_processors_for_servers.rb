5a1da91fc46f201e361c67ea3f9e8555eab6ba53
jbbarth <jeanbaptiste.barth@gmail.com>
2012-04-13 13:49:40 +0200

    Ensure we delete old keys for cpus in Server model

--- db/migrate/20120413070606_structure_processors_for_servers.rb
+++ db/migrate/20120413070606_structure_processors_for_servers.rb
@@ -1,16 +1,17 @@
 class StructureProcessorsForServers < Mongoid::Migration
   def self.up
     Server.all.each do |server|
-      next unless server.attributes["ref_proc"].present? && server.attributes["nb_proc"].present?
-      #old attributes: frequency, nb_proc, nb_coeur, ref_proc
-      #new attributes: processor_X, X in : reference, frequency_GHz, system_count, physical_count, cores_count
-      server.processor_frequency_GHz  = server.attributes["frequency"]
-      server.processor_physical_count = server.attributes["nb_proc"]
-      nb_cores = (server.attributes["nb_coeur"].to_i > 0 ? server.attributes["nb_coeur"] : 1)
-      server.processor_cores_per_cpu  = nb_cores
-      server.processor_system_count   = server.attributes["nb_proc"] * nb_cores
-      server.processor_reference      = server.attributes["ref_proc"]
-      server.save
+      if server.attributes["ref_proc"].present? && server.attributes["nb_proc"].present?
+        #old attributes: frequency, nb_proc, nb_coeur, ref_proc
+        #new attributes: processor_X, X in : reference, frequency_GHz, system_count, physical_count, cores_count
+        server.processor_frequency_GHz  = server.attributes["frequency"]
+        server.processor_physical_count = server.attributes["nb_proc"]
+        nb_cores = (server.attributes["nb_coeur"].to_i > 0 ? server.attributes["nb_coeur"] : 1)
+        server.processor_cores_per_cpu  = nb_cores
+        server.processor_system_count   = server.attributes["nb_proc"] * nb_cores
+        server.processor_reference      = server.attributes["ref_proc"]
+        server.save
+      end
       #delete old things
       server.unset("frequency")
       server.unset("nb_proc")
