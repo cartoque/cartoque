097df61e6f0f92cf78e699ae1949eb28632c31f7
jbbarth <jeanbaptiste.barth@gmail.com>
2012-04-30 11:06:30 +0200

    Removed useless Server.store_in call

--- app/models/server.rb
+++ app/models/server.rb
@@ -7,10 +7,6 @@ class Server
   STATUS_ACTIVE = 1
   STATUS_INACTIVE = 2
 
-  #specify the mongodb collection for now..
-  #TODO: remove it when this class is renamed Server
-  store_in :servers
-
   #standard fields
   field :name, type: String
   field :serial_number, type: String
