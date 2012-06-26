3f145610d6ada1e16b525a23b754fab560ad7d41
jbbarth <jeanbaptiste.barth@gmail.com>
2012-06-26 16:15:18 +0200

    Optimize access to /operating_systems.json

--- app/controllers/operating_systems_controller.rb
+++ app/controllers/operating_systems_controller.rb
@@ -5,18 +5,23 @@ class OperatingSystemsController < InheritedResources::Base
   layout "admin"
 
   def index
-    @operating_systems = OperatingSystem.arrange(order: [:name, :asc])
-    @os_servers_count = Server.all.group_by(&:operating_system_id)
-                                  .inject({}) { |memo,(k,v)| memo[k.to_s] = v.count; memo }
-    @os_servers_count.default = 0
-    if params[:graph_subtree]
-      @root_os = OperatingSystem.find(params[:graph_subtree].to_s)
-      @os_subtree = @root_os.descendants.arrange(order: [:name, :asc]) if @root_os.present?
+    binding.pry
+    if "#{params[:format]}" == "json"
+      @operating_systems = OperatingSystem.all.order_by(:name.asc)
     else
-      @os_subtree = @operating_systems
+      @operating_systems = OperatingSystem.arrange(order: [:name, :asc])
+      @os_servers_count = Server.all.group_by(&:operating_system_id)
+                                    .inject({}) { |memo,(k,v)| memo[k.to_s] = v.count; memo }
+      @os_servers_count.default = 0
+      if params[:graph_subtree]
+        @root_os = OperatingSystem.find(params[:graph_subtree].to_s)
+        @os_subtree = @root_os.descendants.arrange(order: [:name, :asc]) if @root_os.present?
+      else
+        @os_subtree = @operating_systems
+      end
+      @last_updated_server = Server.order_by(:updated_at.desc).first
+      @last_updated_system = OperatingSystem.order_by(:updated_at.desc).first
     end
-    @last_updated_server = Server.order_by(:updated_at.desc).first
-    @last_updated_system = OperatingSystem.order_by(:updated_at.desc).first
   end
 
   def create
